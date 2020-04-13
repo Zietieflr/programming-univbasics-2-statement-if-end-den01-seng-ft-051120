0fc09d7d581fbccf90c3a63cca424b1490f7cb61
Logan McGuire <lrmcguire93@gmail.com>
2020-04-13 22:36:35 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,6 +1,7 @@
 current_time = Time.now;
-if (current_time.to_i / 2 == )
-  puts "Even!"
-else (current_time.to_i / 2 == )
-  puts "Odd!"
+current_time = current_time.to_i;
+if (current_time.even == true )
+  puts "Even!";
+else (current_time.odd == true)
+  puts "Odd!";
 end 
